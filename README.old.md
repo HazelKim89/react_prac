df6f639a517b6bec07067b0e96723db2c51aa81c
hyejiKim <hazel22kim@gmail.com>
2019-01-04 10:55:43 +0900

    basic setting and test simple method

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react_prac
+React Practice
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Restaurant from './Restaurant.js'
+
+
+const restaurants = [
+  {
+    name: "Moxie's Griil & Bar",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5r5WYYtqK85-KYdWk7PakVBV4eYSqrg6y-RWnd6f7Eh5GdLWU"
+  },
+  {
+    name: "Coffe House",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhvJloBLCE50ysv2wNDB78RRjWiW9P-ZrST1PGW8j1LGckqfel"
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {restaurants.map( restaurant, index => {
+          return <Restaurant name={restaurant.name} logo={restaurant.logo} key={index}/>
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
