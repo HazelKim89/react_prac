2f83ad7c59533242930e352516254fb106f809af
hyejiKim <hazel22kim@gmail.com>
2019-02-24 23:58:13 +0900

    following youtube coding day 1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container, Row, Col } from 'reactstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import MainComponents from './base/js/MainMenu.js';
+import Movie from './Movie.js'
 
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/5117ZW5600L._SY445_.jpg",
+  },
+  {
+    title: "Coco",
+    poster: "https://is4-ssl.mzstatic.com/image/thumb/Video118/v4/1a/56/75/1a5675f0-0705-91b9-da49-71a3fe72d0da/contsched.zlfyhaku.lsr/268x0w.jpg",
+  },
+  {
+    title: "Notebook",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51S2j54niLL._SL500_AC_SS350_.jpg",
+  },
+  {
+    title: "La La Land",
+    poster: "http://barkerhost.com/wp-content/uploads/sites/4/2017/01/8SkwS6x3flGZ1p7ATVtYXmnA44o-3.jpg"  
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Container>
-          <h1 className="display-2 text-left">Sketch World</h1>
-          <Row>
-            <Col className="d-block d-lg-none d-xl-none text-right" xs="12">
-              <i className="material-icons">menu</i>
-            </Col>
-            <Col className="d-none d-lg-block d-xl-block" xs="3">
-              <MainComponents.MainMenu />
-            </Col>
-            <Col xs="12" lg="9" xl="9">
-              <MainComponents.MainImgs />
-            </Col>
-          </Row>
-        </Container>
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} /> 
+        })}
       </div>
     );
   }
 }
 
-{/* <div className="conatainer">
-<h1 className="display-2 text-left offset-sm-1">Sketch World</h1>
-  <div className="row">
-    <div className="col-3 d-none d-lg-block d-xl-block">
-      <MainComponents.MainMenu />
-    </div>
-    <div className="col-12 col-lg-9 col-xl-9">
-      <MainComponents.MainImgs />
-    </div>
-  </div>
-</div> */}
 
 export default App;
