4dc54d269b6fb591fcde84ecbf2ad59014da32c3
hyejiKim <hazel22kim@gmail.com>
2019-03-03 22:45:08 +0900

    final education of basic react from youtube video

--- src/App.js
+++ src/App.js
@@ -15,41 +15,38 @@ class App extends Component {
   // this.state.greeting = 'something' --> WRONG
   //should do, just rememeber 'this.setState', after changing state, application will render again
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-      {
-        title: "Matrix",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/5117ZW5600L._SY445_.jpg",
-      },
-      {
-        title: "Coco",
-        poster: "https://is4-ssl.mzstatic.com/image/thumb/Video118/v4/1a/56/75/1a5675f0-0705-91b9-da49-71a3fe72d0da/contsched.zlfyhaku.lsr/268x0w.jpg",
-      },
-      {
-        title: "Notebook",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/51S2j54niLL._SL500_AC_SS350_.jpg",
-      },
-      {
-        title: "La La Land",
-        poster: "http://barkerhost.com/wp-content/uploads/sites/4/2017/01/8SkwS6x3flGZ1p7ATVtYXmnA44o-3.jpg"
-      },
-      {
-        title: "Moulin Rouge",
-        poster: "https://is4-ssl.mzstatic.com/image/thumb/Video69/v4/f9/18/84/f918848f-c696-e40f-33a0-0fd144850ef9/pr_source.lsr/268x0w.png"
-      }
-    ]
-      })
-    }, 1000)
+    this._getMovies()
   }
 
   // the reason of writing underbat in front of function name is to seperate the functions I made
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map( movie => {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres} 
+        synopsis={movie.synopsis}
+      />
     })
     return movies
   }
+
+  _getMovies = async () => {
+    const movies = await this._callApi() // whatever the return value of the function '_callApi' I am going to return value into const 'movies' 
+    this.setState({
+      movies
+    })
+    // ↑ this.setState is going to happen after this._callApi() return the value(doesn't matter returning the value success or not)
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => json.data.movies) // arrow funtion you don't need to use 'return', cause this arrow function return something automatically
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
--- src/Movie.js
+++ src/Movie.js
@@ -17,25 +17,47 @@ import './Movie.css'
 //     )
 //   }
 // }
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
   return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie" >
+      <div className="Movie_Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie_Columns">
+        <h1>{title}</h1>
+        <div className="Movie_Genres">
+          {genres.map((genre,index) => <MovieGenre genre={genre} key={index} /> )}
+        </div>
+        <p className="Movie_Synopsis">
+          {synopsis}
+        </p>
+      </div>
     </div>
   )
 }
 
 // functional component, this is not a class. It's already using props so no need to use 'this.props'.
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
   return (
-    <img src={poster} alt={poster} width="400"/>
+    <img src={poster} alt={alt} width="400" />
     )
+  }
+
+function MovieGenre({genre}){
+  return (
+    <span className="Movie_Genre">{genre}</span>
+  )
+}
+
+  MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
   }
 
   Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
   }
   
   MoviePoster.propTypes = {
